6e99d0bcea6fbcf133c6d73ffa9b8a4400fafe98
Ama-dou <bahamadu2019@gmail.com>
2024-09-07 20:09:31 +0300

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from "./website/HomePage";
 import SearchPage from "./website/SearchPage";
 import ListingDetail from "./website/ListingDetail";
+import NotFound from "./components/NotFound";
 
 const App = () => (
   <Router>
@@ -10,6 +11,7 @@ const App = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/listing/:id" element={<ListingDetail />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
 );
